23a8231df9bbfa1689da9eded0c19423ec494bc0
Marko Valing <marko@radr.eu>
2016-01-12 14:15:58 +0200

    Update config.php

--- rocketchat/config.php
+++ rocketchat/config.php
@@ -14,15 +14,19 @@ class RocketChatPluginConfig extends PluginConfig {
             )),
             'rocketchat-username' => new TextboxField(array(
                 'label' => 'Username',
-                'configuration' => array('size'=>100, 'length'=>200),
+                'configuration' => array('size'=>20, 'length'=>20),
             )),
             'rocketchat-icon_emoji' => new TextboxField(array(
                 'label' => 'Emoji',
-                'configuration' => array('size'=>100, 'length'=>200),
+                'configuration' => array('size'=>20, 'length'=>20),
             )),	
             'rocketchat-alert-text' => new TextboxField(array(
                 'label' => 'Alert text',
-                'configuration' => array('size'=>100, 'length'=>200),
+                'configuration' => array('size'=>20, 'length'=>20),
+            )),	
+            'rocketchat-alert-color' => new TextboxField(array(
+                'label' => 'Alert color',
+                'configuration' => array('size'=>20, 'length'=>20),
             )),	
         );
     }	
